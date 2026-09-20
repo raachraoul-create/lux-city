334c11a21e62ac096798523aa8767d40bf7f3e50
raachraoul-create <raachraoul@gmail.com>
2026-09-20 21:38:56 +0200

    Add bakery production inventory delivery and pricing simulation

---
+++ bakery-data.js
@@ -0,0 +1,30 @@
+export const bakeryProducts=[
+{name:"Baguette tradition",cat:"Pains",min:165,price:2.20,stock:{farine:0.32,levure:0.006,sel:0.006}},
+{name:"Pain de campagne",cat:"Pains",min:240,price:4.60,stock:{farine:0.48,levain:0.12,sel:0.009}},
+{name:"Pain au levain",cat:"Pains",min:900,price:5.20,stock:{farine:0.5,levain:0.15,sel:0.01}},
+{name:"Petit pain",cat:"Pains",min:150,price:0.75,stock:{farine:0.07,levure:0.002,sel:0.001}},
+{name:"Croissant au beurre",cat:"Viennoiseries",min:720,price:1.75,stock:{farine:0.065,beurre:0.032,lait:0.025,levure:0.002}},
+{name:"Pain au chocolat",cat:"Viennoiseries",min:720,price:1.95,stock:{farine:0.07,beurre:0.032,chocolat:0.018,levure:0.002}},
+{name:"Brioche",cat:"Viennoiseries",min:210,price:3.60,stock:{farine:0.22,beurre:0.045,oeufs:0.08,lait:0.08}},
+{name:"Quetschentaart",cat:"Spécialités luxembourgeoises",min:135,price:18.50,stock:{farine:0.25,beurre:0.12,oeufs:0.1,sucre:0.12,quetsches:0.8}},
+{name:"Boxemännchen",cat:"Spécialités luxembourgeoises",min:85,price:3.20,stock:{farine:0.1,levure:0.004,sucre:0.012,lait:0.05,oeufs:0.04,beurre:0.02}},
+{name:"Bretzel luxembourgeoise",cat:"Spécialités luxembourgeoises",min:600,price:5.50,stock:{farine:0.16,beurre:0.08,sucre:0.05,amandes:0.025}},
+{name:"Verwurelter",cat:"Spécialités luxembourgeoises",min:120,price:2.70,stock:{farine:0.1,oeufs:0.03,sucre:0.02,huile:0.03}},
+{name:"Bamkuch",cat:"Spécialités luxembourgeoises",min:300,price:34.00,stock:{farine:0.35,beurre:0.35,oeufs:0.4,sucre:0.35}},
+{name:"Rieslingspaschtéit",cat:"Salé",min:240,price:8.90,stock:{farine:0.18,beurre:0.08,viande:0.25,riesling:0.05}},
+{name:"Quiche lorraine",cat:"Salé",min:105,price:5.90,stock:{farine:0.09,beurre:0.04,oeufs:0.08,creme:0.08,lardons:0.06}},
+{name:"Sandwich jambon-fromage",cat:"Snacking",min:20,price:5.80,stock:{farine:0.12,jambon:0.06,fromage:0.04}}
+];
+export const supplies=[
+{name:"Farine de blé T55",key:"farine",unit:"25 kg",qty:25,price:23.90},
+{name:"Farine de seigle",key:"seigle",unit:"25 kg",qty:25,price:29.90},
+{name:"Beurre professionnel",key:"beurre",unit:"10 kg",qty:10,price:89.00},
+{name:"Sucre",key:"sucre",unit:"25 kg",qty:25,price:28.50},
+{name:"Sel",key:"sel",unit:"25 kg",qty:25,price:12.50},
+{name:"Levure boulangère",key:"levure",unit:"5 kg",qty:5,price:31.00},
+{name:"Lait entier",key:"lait",unit:"12 L",qty:12,price:15.60},
+{name:"Œufs",key:"oeufs",unit:"180 pièces",qty:10.8,price:39.00},
+{name:"Chocolat",key:"chocolat",unit:"5 kg",qty:5,price:58.00},
+{name:"Amandes",key:"amandes",unit:"5 kg",qty:5,price:49.00},
+{name:"Quetsches",key:"quetsches",unit:"10 kg",qty:10,price:32.00}
+];
